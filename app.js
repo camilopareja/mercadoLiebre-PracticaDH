874c467fad262cdb234ba9d428722eb90ccd0509
camilopareja <camilopareja11@gmail.com>
2022-04-12 21:22:30 -0500

    se agrego y conectó con app.js el html crear-cuenta

--- app.js
+++ app.js
@@ -7,5 +7,9 @@ app.use('/static', express.static(__dirname + '/public'));
 app.listen(3030, console.log('Servidor corriendo'));
 
 app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, "/views/home.HTML"))
+    res.sendFile(__dirname + "/views/home.HTML")
+});
+
+app.get("/crear-cuenta", (req,res) => {
+    res.sendFile(__dirname + "/views/crear-cuenta.html")
 });
