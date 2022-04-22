17aaec5da38f2df48845041f3356d8ab7a20cca9
camilopareja <camilopareja11@gmail.com>
2022-04-21 21:03:07 -0500

    configurando las carpetaspara hacer el deploy

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const path = require('path')
 
 app.use('/static', express.static(__dirname + '/public'));
 
-app.listen(3030, console.log('Servidor corriendo'));
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Servidor corriendo');
+});
 
 app.get("/", (req,res) => {
     res.sendFile(__dirname + "/views/home.HTML")
